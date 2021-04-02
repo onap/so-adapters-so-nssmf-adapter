7b6b1afaaa9928b97e2cee329607258daebedc92
mukesh.paliwal <mukesh.paliwal1@huawei.com>
2021-04-02 15:59:42 +0530

    docker create failed for so-nssmf-adapter Issue-ID: SO-3611
    Signed-off-by: mukesh.paliwal <mukesh.paliwal1@huawei.com>
    Change-Id: I76d86cf4a14eb5711b2adf8cb09540ea7ac0d82a

--- packages/docker/src/main/docker/docker-files/scripts/start-app.sh
+++ packages/docker/src/main/docker/docker-files/scripts/start-app.sh
@@ -1,36 +1,49 @@
 #!/bin/sh
-#temprary condition
+
 if [ `id -u` = 0 ]
 then
-	# Install certificates found in the /app/ca-certificates volume, if any.
+    # Install certificates found in the /app/ca-certificates volume, if any.
 
-	needUpdate=FALSE
+    needUpdate=FALSE
 
-	for certificate in `ls -1 /app/ca-certificates`; do
-		echo "Installing $certificate in /usr/local/share/ca-certificates"
-		# cp /app/ca-certificates/$certificate /usr/local/share/ca-certificates/$certificate
-		needUpdate=FALSE
-	done
+    for certificate in `ls -1 /app/ca-certificates`; do
+        echo "Installing $certificate in /usr/local/share/ca-certificates"
+        cp /app/ca-certificates/$certificate /usr/local/share/ca-certificates/$certificate
+        needUpdate=TRUE
+    done
 
-	# Re-exec this script as the 'onap' user.
-	this=`readlink -f $0`
-	# exec su so -c  "$this"
+    if [ $needUpdate = TRUE ]; then
+        update-ca-certificates --fresh
+    fi
+
+    # Re-exec this script as the 'onap' user.
+    this=`readlink -f $0`
+    exec su so -c  "$this"
 fi
 
 touch /app/app.jar
 
+if [ -z "$APP" ]; then
+    echo "CONFIG ERROR: APP environment variable not set"
+    exit 1
+fi
+
 if [ ! -z "$DB_HOST" -a -z "$DB_PORT" ]; then
-	export DB_PORT=3306
+    export DB_PORT=3306
 fi
 
 if [ -z "${CONFIG_PATH}" ]; then
-	export CONFIG_PATH=/app/config/override.yaml
+    export CONFIG_PATH=/app/config/override.yaml
+fi
+
+if [ -z "${LOG_PATH}" ]; then
+    export LOG_PATH="logs/${APP}"
 fi
 
 if [ "${SSL_DEBUG}" = "log" ]; then
-	export SSL_DEBUG="-Djavax.net.debug=all"
+    export SSL_DEBUG="-Djavax.net.debug=all"
 else
-	export SSL_DEBUG=
+    export SSL_DEBUG=
 fi
 
 # Set java keystore and truststore options, if specified in the environment.
@@ -38,18 +51,18 @@ fi
 jksargs=
 
 if [ ! -z "${KEYSTORE}" ]; then
-	jksargs="$jksargs -Dmso.load.ssl.client.keystore=true"
-	jksargs="$jksargs -Djavax.net.ssl.keyStore=$KEYSTORE"
-	jksargs="$jksargs -Djavax.net.ssl.keyStorePassword=${KEYSTORE_PASSWORD}"
+    jksargs="$jksargs -Dmso.load.ssl.client.keystore=true"
+    jksargs="$jksargs -Djavax.net.ssl.keyStore=$KEYSTORE"
+    jksargs="$jksargs -Djavax.net.ssl.keyStorePassword=${KEYSTORE_PASSWORD}"
 fi
 
 if [ ! -z "${TRUSTSTORE}" ]; then
-	jksargs="$jksargs -Djavax.net.ssl.trustStore=${TRUSTSTORE}"
-	jksargs="$jksargs -Djavax.net.ssl.trustStorePassword=${TRUSTSTORE_PASSWORD}"
+    jksargs="$jksargs -Djavax.net.ssl.trustStore=${TRUSTSTORE}"
+    jksargs="$jksargs -Djavax.net.ssl.trustStorePassword=${TRUSTSTORE_PASSWORD}"
 fi
 
 if [ -z "${ACTIVE_PROFILE}" ]; then
-	export ACTIVE_PROFILE="basic"
+    export ACTIVE_PROFILE="basic"
 fi
 
 jvmargs="${JVM_ARGS} -Dspring.profiles.active=${ACTIVE_PROFILE} -Djava.security.egd=file:/dev/./urandom -Dlogs_dir=${LOG_PATH} -Dlogging.config=/app/logback-spring.xml $jksargs -Dspring.config.additional-location=$CONFIG_PATH ${SSL_DEBUG} ${DISABLE_SNI}"
@@ -68,8 +81,6 @@ read_properties(){
 	EOF
 }
 
-
-
 if [ -n "${AAF_SSL_CERTS_ENABLED}" ]; then
 read_properties "$(head -n 4 /app/certs/.passphrases)"
 fi
@@ -82,8 +93,8 @@ rc=$?
 echo "Application exiting with status code $rc"
 
 if [ ! -z "${EXIT_DELAY}" -a "${EXIT_DELAY}" != 0 ]; then
-	echo "Delaying $APP exit for $EXIT_DELAY seconds"
-	sleep $EXIT_DELAY
+    echo "Delaying $APP exit for $EXIT_DELAY seconds"
+    sleep $EXIT_DELAY
 fi
 
 exit $rc
